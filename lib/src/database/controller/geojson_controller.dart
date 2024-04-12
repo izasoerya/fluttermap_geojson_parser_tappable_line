ce3bc41870fb68c2d8b48f8a0967fa367c1fd857
izasoerya <ihza.surya@gmail.com>
2024-04-12 14:55:43 +0700

    update database

--- lib/src/database/controller/geojson_controller.dart
+++ lib/src/database/controller/geojson_controller.dart
@@ -1,20 +1,24 @@
 import 'package:flutter_map_geojson/flutter_map_geojson.dart';
 import 'package:kunci_determinasi/src/database/models/dart_data_sleman.dart';
-import 'package:kunci_determinasi/src/database/models/dart_dummy.dart';
+// import 'package:kunci_determinasi/src/database/models/dart_dummy.dart';
+import 'package:kunci_determinasi/src/database/models/dart_semarang.dart';
+
+final List<dynamic> geojsonProperty = [];
 
 class GeoParser {
   const GeoParser({required this.file});
   final int file;
 
   static final parser = GeoJsonParser();
+
   List<dynamic> getListOfLatLng() {
-    // dynamic is either polyline or polygon
+    // dynamic is ALWAYS either polyline or polygon
     switch (file) {
       case 1:
         parser.parseGeoJsonAsString(dataSleman);
         return parser.polylines;
       case 2:
-        parser.parseGeoJsonAsString(dataDummy);
+        parser.parseGeoJsonAsString(dataSemarang);
         return parser.polygons;
     }
     return parser.polylines; // should not be reached
